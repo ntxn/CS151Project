f5d62bd7df55417d9540666f6a68305a6849f3a6
Kiera Nguyen <ngannguyen9017@gmail.com>
2018-11-20 17:32:30 -0800

    Split HRS into HRS & Tester. Flipping JPanels works

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -8,41 +8,34 @@ import javax.swing.*;
 import javax.swing.event.*;
 /**
  * NEED TO ADD FUNCTION TO BACK BUTTON OF view resersation by room
- * @author Kiera
+ * @author Ngan Nguyen
  *
  */
-public class HotelReservationSystem {
-	private static Hashtable guests = new Hashtable(); // hold existing guest info from guests.txt
-	private static ArrayList<Reservation> all_reservations = new ArrayList<Reservation>(); // hold all reservations from reservations.txt
-    private static ArrayList<Room> rooms = new ArrayList<Room>(); // hold all general rooms info from rooms.txt
-    private static Hashtable roomsByHashtable = new Hashtable();
-    private static Hashtable catagorizedRooms = new Hashtable();
+public class HotelReservationSystem extends JFrame{
+	private static Hashtable guests = new Hashtable(); 
+	// hold existing guests info from guests.txt
+	private static ArrayList<Reservation> reservations = new ArrayList<Reservation>(); 
+		// hold all reservations from reservations.txt
+	private static ArrayList<Room> rooms = new ArrayList<Room>(); 
+		// hold all general rooms info from rooms.txt
+	private static Hashtable categorizedRooms = new Hashtable();
+	private static Hashtable roomsByHashtable = new Hashtable();
     private static Guest currentUser; // keep track of which user is using the program
     
+    public HotelReservationSystem(Hashtable guests, ArrayList<Reservation> reservations,
+    		ArrayList<Room> rooms, Hashtable catagorizedRooms, Hashtable roomsByHashtable){
     
-    
-	public static void main(String[] args) throws FileNotFoundException{
-		loadData("guests.txt", 1);
-		loadData("rooms.txt", 2);
-		loadData("reservations.txt", 3);
-		currentUser = (Guest) guests.get("Luweiwei");
-		
-		JFrame frame = new JFrame("Hotel SEN");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(700, 500);
-		
-		/* TEST CODE FOR LOADING DATA 
-        System.out.println(guests.get("ngannguyen2").toString());
-		
-        for(Room room : rooms)
-        	System.out.println(room.toString());
-        
-        for(Reservation r: all_reservations)
-        	System.out.println(r.toString());
-        */
-		
-	//MVC - VIEW RESERVATION BY ROOM	
-		ReservationsByRoomModel reservationsByRoomModel = new ReservationsByRoomModel(all_reservations);
+    	// INITIALIZE variables	
+    	this.guests = guests;
+    	this.reservations = reservations;
+    	this.rooms = rooms;
+    	this.categorizedRooms = catagorizedRooms;
+    	this.roomsByHashtable = roomsByHashtable;
+    	currentUser = (Guest)guests.get("ngannguyen");
+    	
+    	
+    //MVC - VIEW RESERVATION BY ROOM	
+		ReservationsByRoomModel reservationsByRoomModel = new ReservationsByRoomModel(reservations);
 		ReservationsByRoomPanel reservationsByRoomPanel = new ReservationsByRoomPanel(reservationsByRoomModel, rooms);
 		reservationsByRoomModel.addChangeListener(reservationsByRoomPanel);
 		JPanel viewReservationByRoomPANEL = new JPanel(new BorderLayout());
@@ -54,7 +47,7 @@ public class HotelReservationSystem {
 		viewReservationByRoomPANEL.add(viewReservationByRoomButtonPanel, BorderLayout.SOUTH);
 		
 	//MVC - MAKING A RESERVATION	
-		BookedRoomsByDatesModel bookedRoomsByDatesModel = new BookedRoomsByDatesModel(all_reservations, catagorizedRooms, currentUser, roomsByHashtable);
+		BookedRoomsByDatesModel bookedRoomsByDatesModel = new BookedRoomsByDatesModel(reservations, categorizedRooms, currentUser, roomsByHashtable);
 		
 		// VIEW & CONTROLLER 1
 		GetBookingInfoPanel getBookingInfoPanel = new GetBookingInfoPanel(bookedRoomsByDatesModel);
@@ -89,7 +82,7 @@ public class HotelReservationSystem {
 		bookingPanel.add(endReservationPanel, BorderLayout.SOUTH);
 		
 	//VIEW/CANCEL RESERVATIONS BY GUEST
-		ViewCancelReservations viewCancelReservations = new ViewCancelReservations(all_reservations, currentUser);
+		ViewCancelReservations viewCancelReservations = new ViewCancelReservations(reservations, currentUser);
 		// Add a scroll
 		JScrollPane scroll = new JScrollPane(viewCancelReservations, 
 				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, 
@@ -103,110 +96,37 @@ public class HotelReservationSystem {
 		ReceiptFormatter formatter = new SimpleFormatter(); // TESTER FOR RECEIPT
 		
 		
-		cancelReservation_QUIT_Button.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){ // FOR TEMPORARY TESTING ONLY
-				/*System.out.println("\n" + all_reservations.size());
-				for(int i=0; i < all_reservations.size(); i++)
-					System.out.println("\n#" + i+ "\n" + all_reservations.get(i).toString());
-					*/
-				//System.out.println(viewCancelReservations.printReceipt(formatter));
-				
-				frame.add(viewReservationByRoomPANEL);
-				//frame.dispose();
-				
-			}
-		});
+		
 		
 		JPanel cancelReservationButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		cancelReservationButtonPanel.add(cancelReservation_QUIT_Button);
 		cancelReservationsPanel.add(cancelReservationButtonPanel, BorderLayout.SOUTH);
 				
 		
-	
+	// PANEL to hold all PAGES
+		// Layout allows us to flip through different JPANEL
+		JPanel pages = new JPanel(new CardLayout());
+		pages.add(cancelReservationsPanel, "guest");
+		pages.add(bookingPanel, "booking");
+		pages.add(viewReservationByRoomPANEL, "manager");
+		
+		CardLayout cardLayout = (CardLayout) pages.getLayout();
+		
+	// ADD ActionListener to each button to flip through different pages
+		cancelReservation_QUIT_Button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+
+				//System.out.println(viewCancelReservations.printReceipt(formatter));
+				
+				cardLayout.show(pages, "booking");
+				
+			}
+		});
 		
 		
 		
 		
-		
-		//frame.add(viewReservationByRoomPANEL);
-		//frame.add(bookingPanel);
-		frame.add(cancelReservationsPanel);
-		
-		frame.setVisible(true);
+		add(pages);
     }
 	
-	/**
-	 * Load data from a text file to data structure in Java
-	 * @param fileName
-	 * @param option 1: guests.txt, 2: rooms.txt, 3: reservations.txt
-	 * @throws FileNotFoundException
-	 */
-	public static void loadData(String fileName, int option) throws FileNotFoundException{
-		File file = new File(fileName);
-		Scanner fin = new Scanner(file);
-		
-		switch(option){
-		case 1:
-			/* LOAD GUESTS INFO */
-			while(fin.hasNextLine()){
-				String name = fin.nextLine(); 
-				String[] logIn = fin.nextLine().split(" ");
-				guests.put(logIn[0], new Guest(name, logIn[0], logIn[1]));
-			}
-			break;
-		case 2:
-			/* LOAD ROOMS INFO */
-			ArrayList<Room> type1Rooms = new ArrayList<Room>();
-		    ArrayList<Room> type2Rooms = new ArrayList<Room>();
-		    ArrayList<Room> type3Rooms = new ArrayList<Room>();
-		    
-			while(fin.hasNextLine()){
-				String[] roomInfo = fin.nextLine().split(" ");
-				int room_number = Integer.parseInt(roomInfo[0]);
-				int price = Integer.parseInt(roomInfo[1]);
-				Room room = new Room(room_number, price);
-				rooms.add(room);
-				
-				roomsByHashtable.put(room_number, room);
-				
-				if(price == 100)
-					type1Rooms.add(room);
-				else if(price == 200)
-					type2Rooms.add(room);
-				else
-					type3Rooms.add(room);
-			}
-			
-			catagorizedRooms.put(100, type1Rooms);
-			catagorizedRooms.put(200, type2Rooms);
-			catagorizedRooms.put(300, type3Rooms);
-			break;
-		case 3:
-			/* LOAD RESERVATIONS INFO */
-			while(fin.hasNextLine()){
-				String[] str = fin.nextLine().split(" ");
-				
-				String[] date = str[2].split("/");
-				LocalDate start_date = LocalDate.of(Integer.parseInt(date[2]), 
-						Integer.parseInt(date[0]), Integer.parseInt(date[1]));
-				
-				date = str[3].split("/");
-				LocalDate end_date = LocalDate.of(Integer.parseInt(date[2]), 
-						Integer.parseInt(date[0]), Integer.parseInt(date[1]));
-				
-				date = str[5].split("/");
-				LocalDate bookingDate = LocalDate.of(Integer.parseInt(date[2]), 
-						Integer.parseInt(date[0]), Integer.parseInt(date[1]));
-				DateInterval dateInterval = new DateInterval(start_date, end_date);
-				
-				int room_number = Integer.parseInt(str[1]);
-				
-				all_reservations.add(new Reservation((Guest)guests.get(str[0]), (Room)roomsByHashtable.get(room_number), 
-						dateInterval, Integer.parseInt(str[4]), bookingDate));
-			} 
-			break;
-		}
-		
-		fin.close();
-	}
 }
