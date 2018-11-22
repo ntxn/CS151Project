59a5a7201c1ddcc526ecc6b3f49baba97cdacd46
macbookpro <macbookpro@macbookprodeMacBook-Pro.local>
2018-11-21 20:28:44 -0800

    add calendar model,view

---
+++ src/CalendarModel.java
@@ -0,0 +1,164 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * Model for Calendar. 
+ */
+public class CalendarModel{
+
+	private int maxDays;
+	private int selectedDay;
+	private ArrayList<ChangeListener> listeners = new ArrayList<>();
+	private GregorianCalendar cal = new GregorianCalendar();
+	private boolean monthChanged = false;
+	
+	/**
+	 * Constructor
+	 */
+	public CalendarModel() {
+		maxDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		selectedDay = cal.get(Calendar.DATE);
+		//loadEvents();
+	}
+	
+	/**
+	 * Adds ChangeListeners to array.
+	 * @param l the ChangeListener
+	 */
+	public void attach(ChangeListener l) {
+		listeners.add(l);
+	}
+	
+	/**
+	 * Updates all ChangeListeners in array.
+	 */
+	public void update() {
+		for (ChangeListener l : listeners) {
+			l.stateChanged(new ChangeEvent(this));
+		}
+	}
+	
+	/**
+	 * Sets the user selected day.
+	 * @param day the day of the month
+	 */
+	public void setSelectedDate(int day) {
+		selectedDay = day;
+	}
+	
+	/**
+	 * Gets the user selected day.
+	 * @return the day of the month
+	 */
+	public int getSelectedDay() {
+		return selectedDay;
+	}
+
+	/**
+	 * Gets the current year the calendar is at.
+	 * @return the current year
+	 */
+	public int getCurrentYear() {
+		return cal.get(Calendar.YEAR);
+	}
+	
+	/**
+	 * Gets the current month the calendar is at.
+	 * @return the current month
+	 */
+	public int getCurrentMonth() {
+		return cal.get(Calendar.MONTH);
+	}
+	
+	/**
+	 * Gets the value representing the day of the week
+	 * @param i the day of the month
+	 * @return the day of the week (1-7)
+	 */
+	public int getDayOfWeek(int i) {
+		cal.set(Calendar.DAY_OF_MONTH, i);
+		return cal.get(Calendar.DAY_OF_WEEK);
+	}
+	
+	/**
+	 * Gets the max number of days in a month.
+	 * @return the max number of days in a month
+	 */
+	public int getMaxDays() {
+		return maxDays;
+	}
+
+	/**
+	 * Moves the calendar forward by one month.
+	 */
+	public void nextMonth() {
+		cal.add(Calendar.MONTH, 1);
+		maxDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		monthChanged = true;
+		update();
+	}
+	
+	/**
+	 * Moves the calendar backward by one month.
+	 */
+	public void prevMonth() {
+		cal.add(Calendar.MONTH, -1);
+		maxDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		monthChanged = true;
+		update();
+	}
+	
+	/**
+	 * Moves the selected day forward by one.
+	 */
+	public void nextDay() {
+		selectedDay++;
+		if (selectedDay > cal.getActualMaximum(Calendar.DAY_OF_MONTH)) {
+			nextMonth();
+			selectedDay = 1;
+		}
+		update();
+	}
+	
+	/**
+	 * Moves the selected day backward by one.
+	 */
+	public void prevDay() {
+		selectedDay--;
+		if (selectedDay < 1) {
+			prevMonth();
+			selectedDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		}
+		update();
+	}
+	
+	/**
+	 * Checks if the month has changed as a result of user interaction.
+	 * @return
+	 */
+	public boolean hasMonthChanged() {
+		return monthChanged;
+	}
+	
+	/**
+	 * Resets monthChanged to false.
+	 */
+	public void resetHasMonthChanged() {
+		monthChanged = false;
+	}
+	
+}
---
+++ src/CalendarView.java
@@ -0,0 +1,242 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+
+
+
+public class CalendarView implements ChangeListener {
+
+	public enum DAYS {
+		Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday;
+	}
+	
+	public enum MONTHS {
+		January, February, March, April, May, June, July, August, September, October, November, December;
+	}
+	private CalendarModel model;
+	private DAYS[] arrayOfDays = DAYS.values();
+	private MONTHS[] arrayOfMonths = MONTHS.values();
+	private int prevHighlight = -1;
+	private int maxDays;
+
+	private JFrame frame = new JFrame("Calendar");
+	private JPanel monthViewPanel = new JPanel();
+	private JLabel monthLabel = new JLabel();
+	//private JButton create = new JButton("Create");
+	private JButton nextDay = new JButton("Next");
+	private JButton prevDay = new JButton("Prev");
+	private JTextPane dayTextPane = new JTextPane();
+	private ArrayList<JButton> dayButtons = new ArrayList<JButton>();
+
+	/**
+	 * Constructs the calendar.
+	 * @param model the  model that stores and manipulates calendar data
+	 */
+	public CalendarView(CalendarModel model) {
+		this.model = model;
+		maxDays = model.getMaxDays();
+		monthViewPanel.setLayout(new GridLayout(0, 7));
+		dayTextPane.setPreferredSize(new Dimension(300, 150));
+		dayTextPane.setEditable(false);
+
+		createDayButtons();
+		addBlankButtons();
+		addDayButtons();
+		//highlightEvents();
+		showDate(model.getSelectedDay());
+		highlightSelectedDate(model.getSelectedDay() - 1);
+
+		/*create.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				createEventDialog();
+			}
+		});*/
+		JButton prevMonth = new JButton("<");
+		prevMonth.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				model.prevMonth();
+				//create.setEnabled(false);
+				nextDay.setEnabled(false);
+				prevDay.setEnabled(false);
+				dayTextPane.setText("");
+			}
+		});
+		JButton nextMonth = new JButton(">");
+		nextMonth.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				model.nextMonth();
+				//create.setEnabled(false);
+				nextDay.setEnabled(false);
+				prevDay.setEnabled(false);
+				dayTextPane.setText("");
+			}
+		});
+		
+		JPanel monthContainer = new JPanel();
+		monthContainer.setLayout(new BorderLayout());
+		monthLabel.setText(arrayOfMonths[model.getCurrentMonth()] + " " + model.getCurrentYear());
+		monthContainer.add(monthLabel, BorderLayout.NORTH);
+		monthContainer.add(new JLabel("     S             M             T             W             T              F             S"), BorderLayout.CENTER);
+		monthContainer.add(monthViewPanel, BorderLayout.SOUTH);
+		
+		JPanel dayViewPanel = new JPanel();
+		dayViewPanel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		JScrollPane dayScrollPane = new JScrollPane(dayTextPane);
+		dayScrollPane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		dayViewPanel.add(dayScrollPane, c);
+		JPanel buttonsPanel = new JPanel();
+		nextDay.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				model.nextDay();
+			}
+		});
+		prevDay.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				model.prevDay();
+			}
+		});
+		buttonsPanel.add(prevDay);
+		//buttonsPanel.add(create);
+		buttonsPanel.add(nextDay);
+		c.gridx = 0;
+		c.gridy = 1;
+		dayViewPanel.add(buttonsPanel, c);
+
+		JButton quit = new JButton("Quit");
+		quit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//model.saveEvents();
+				System.exit(0);
+			}
+		});
+
+		frame.add(prevMonth);
+		frame.add(monthContainer);
+		frame.add(nextMonth);
+		frame.add(dayViewPanel);
+		frame.add(quit);
+		frame.setLayout(new FlowLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		if (model.hasMonthChanged()) {
+			maxDays = model.getMaxDays();
+			dayButtons.clear();
+			monthViewPanel.removeAll();
+			monthLabel.setText(arrayOfMonths[model.getCurrentMonth()] + " " + model.getCurrentYear());
+			createDayButtons();
+			addBlankButtons();
+			addDayButtons();
+			//highlightEvents();
+			prevHighlight = -1;
+			model.resetHasMonthChanged();
+			frame.pack();
+			frame.repaint();
+		} else {
+			showDate(model.getSelectedDay());
+			highlightSelectedDate(model.getSelectedDay() - 1);
+		}
+	}
+
+	
+
+	/**
+	 * Shows the selected date and events on that date.
+	 * @param d the selected date
+	 */
+	private void showDate(final int d) {
+		model.setSelectedDate(d);
+		String dayOfWeek = arrayOfDays[model.getDayOfWeek(d) - 1] + "";
+		String date = (model.getCurrentMonth() + 1) + "/" + d + "/" + model.getCurrentYear();
+		String events = "";
+		/*if (model.hasEvent(date)) {
+			events += model.getEvents(date);
+		}*/
+		dayTextPane.setText(dayOfWeek + " " + date + "\n" + events);
+		dayTextPane.setCaretPosition(0);
+	}
+
+	/**
+	 * Highlights the currently selected date.
+	 * @param d the currently selected date
+	 */
+	private void highlightSelectedDate(int d) {
+		Border border = new LineBorder(Color.ORANGE, 2);
+		dayButtons.get(d).setBorder(border);
+		if (prevHighlight != -1) {
+			dayButtons.get(prevHighlight).setBorder(new JButton().getBorder());
+		}
+		prevHighlight = d;
+	}
+
+	
+
+	/**
+	 * Creates buttons representing days of the current month and adds them to an array list.
+	 */
+	private void createDayButtons() {
+		for (int i = 1; i <= maxDays; i++) {
+			final int d = i;
+			JButton day = new JButton(Integer.toString(d));
+			day.setBackground(Color.WHITE);
+	
+			day.addActionListener(new ActionListener() {
+	
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					showDate(d);
+					highlightSelectedDate(d - 1);
+					//create.setEnabled(true);
+					nextDay.setEnabled(true);
+					prevDay.setEnabled(true);
+				}
+			});
+			dayButtons.add(day);
+		}
+	}
+
+	/**
+	 * Adds the buttons representing the days of the month to the panel.
+	 */
+	private void addDayButtons() {
+		for (JButton d : dayButtons) {
+			monthViewPanel.add(d);
+		}
+	}
+
+	/**
+	 * Adds filler buttons before the start of the month to align calendar.
+	 */
+	private void addBlankButtons() {
+		for (int j = 1; j < model.getDayOfWeek(1); j++) {
+			JButton blank = new JButton();
+			blank.setEnabled(false);
+			monthViewPanel.add(blank);
+		}
+	}
+}
